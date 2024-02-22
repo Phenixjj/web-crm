7bc6e1a119151b39268eaca8ac1a1f24d4bd5fab
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-02-22 13:45:14 +0100

    feat: Create & DL Invoices pdf

--- webcrm/sales/models.py
+++ webcrm/sales/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 from products.models import Product
-from products.tools.utils import random_slug_generator
+from products.tools.utils import random_slug_generator, random_integer_generator
+
+import datetime as dt
+from django.utils import timezone
 
 
 # Create your models here.
@@ -61,3 +64,46 @@ class Order(models.Model):
     @property
     def get_total(self):
         return self.product.price * self.quantity
+
+
+class Invoice(models.Model):
+    id = models.AutoField(primary_key=True)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    status = models.CharField(max_length=50, default='PENDING')
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    invoice_order_id = models.SlugField(max_length=230)
+    json = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Invoice for {self.order.order_id}"
+
+    def save(self, *args, **kwargs):
+
+        if not self.total:
+            self.total = self.order.total
+
+        if not self.due_date:
+            self.due_date = dt.datetime.now() + dt.timedelta(days=60)
+
+        if not self.invoice_order_id:
+            current_date = timezone.now()
+            date_string = current_date.strftime("%Y-%m-%d")
+            self.invoice_order_id = f"{date_string.replace('-', '')}-{str(self.id)}"
+
+        if not self.json:
+            self.json = self.to_json()
+        super(Invoice, self).save(*args, **kwargs)
+
+    def to_json(self):
+        return {
+            "order_id": str(self.order.order_id),
+            "product": str(self.order.product.name),
+            "'quantity": str(self.order.quantity),
+            "invoice_id": str(self.invoice_order_id),
+            "total": str(self.total),
+            "due_date": str(self.due_date),
+            "status": str(self.order.status),
+            "date_created": str(self.date_created)
+        }
---
+++ webcrm/sales/pdf.py
@@ -0,0 +1,37 @@
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from django.conf import settings
+from PIL import Image
+import os
+
+
+class PDF:
+    def __init__(self, file_name):
+        self.file_name = f"{file_name}.pdf"
+
+    def create_invoice(self, invoice):
+        # Create a PDF document
+        c = canvas.Canvas(self.file_name, pagesize=letter)
+        width, height = letter
+
+        # Add some custom text for headers
+        c.setFont("Helvetica-Bold", 24)
+        c.drawString(30, height - 50, "Your Invoice from Django Sales")
+        c.setFont("Helvetica", 12)
+        logo_path = os.path.join(settings.BASE_DIR, 'static', 'assets', 'logo-crm2.png')
+        with Image.open(logo_path) as img:
+            original_width, original_height = img.size
+
+            # Calculate the new logo dimensions
+        aspect_ratio = original_width / original_height
+        new_width = 80
+        new_height = new_width / aspect_ratio
+
+        c.drawImage(logo_path, 500, 700, width=new_width, height=new_height)
+        c.drawString(60, 710, f"Invoice for {invoice.order.order_id}")
+        c.drawString(60, 690, f"Date Created: {invoice.date_created}")
+        c.drawString(60, 670, f"Due Date: {invoice.due_date}")
+        c.drawString(60, 650, f"Status: {invoice.order.status}")
+        c.drawString(60, 620, f"Total: {invoice.total} EUR")
+        c.save()
+        return self.file_name
