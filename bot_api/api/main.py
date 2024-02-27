967dc693fcc5a0fd4d49b3315f679c10b3d7bcab
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-02-27 21:08:23 +0100

    feat: WIP bot_assist fastAPI & Ollama service setup

---
+++ bot_api/api/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from bot import bot
+
+
+class MessageInput(BaseModel):
+    prompt: str
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+# route to test the bot with a prompt with input message from the user pass in the generated response
+@app.post("/message/")
+def message(input_user: MessageInput):
+    try:
+        response = bot.generate_response(input_user.prompt)
+        return {"response": response}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
