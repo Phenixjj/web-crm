d482cf493fdb1c5d4006c02127f81c9e1d6847b6
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-03-01 13:31:32 +0100

    feat: simple chat teams

--- webcrm/chat/views.py
+++ webcrm/chat/views.py
@@ -1,3 +1,7 @@
+import json
+
+from django.contrib.auth.decorators import login_required
+from django.utils.safestring import mark_safe
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,5 +11,9 @@ def chat_lobby(request):
     return render(request, "chat/chat.html")
 
 
+@login_required
 def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+    return render(request, "chat/room.html", {
+        "room_name": room_name,
+        "username": mark_safe(json.dumps(request.user.username)),
+    })
