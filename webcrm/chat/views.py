91f4f713decddd6b2fced293d0fb5f6225d79294
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-03-08 16:42:31 +0100

    feat: BIG WIP

--- webcrm/chat/views.py
+++ webcrm/chat/views.py
@@ -1,14 +1,18 @@
 import json
+import random
+import time
 
+from agora_token_builder import RtcTokenBuilder
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
+from django.views.decorators.csrf import csrf_exempt
 
 from webcrm.env import config
 
-from .models import Chat
+from .models import Chat, RoomMember
 
 # Create your views here.
 
@@ -85,5 +89,53 @@ def room(request, room_name):
 
 
 def getToken(request):
-    str(config("AGORA_APP_ID"))
-    str(config("AGORA_APP_CERTIFICATE"))
+    appId = str(config("AGORA_APP_ID"))
+    appCertificate = str(config("AGORA_APP_CERTIFICATE"))
+    channelName = request.GET.get("channel")
+    uid = random.randint(1, 230)
+    expirationTimeInSeconds = 3600
+    currentTimeStamp = int(time.time())
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
+
+    token = RtcTokenBuilder.buildTokenWithUid(
+        appId, appCertificate, channelName, uid, role, privilegeExpiredTs
+    )
+
+    return JsonResponse({"token": token, "uid": uid}, safe=False)
+
+
+@csrf_exempt
+def createMember(request):
+    data = json.loads(request.body)
+    member, created = RoomMember.objects.get_or_create(
+        name=data["name"], uid=data["UID"], room_name=data["room_name"]
+    )
+
+    return JsonResponse({"name": data["name"]}, safe=False)
+
+
+def getMember(request):
+    uid = request.GET.get("UID")
+    room_name = request.GET.get("room_name")
+
+    member = RoomMember.objects.get(
+        uid=uid,
+        room_name=room_name,
+    )
+    member.name
+    return JsonResponse({"name": member.name}, safe=False)
+
+
+@csrf_exempt
+def deleteMember(request):
+    data = json.loads(request.body)
+    member = RoomMember.objects.get(
+        name=data["name"], uid=data["UID"], room_name=data["room_name"]
+    )
+    member.delete()
+    return JsonResponse("Member deleted", safe=False)
+
+
+# def video_chat(request):
+#     return render(request, "chat/video_chat.html")
--- webcrm/static/js/streams.js
+++ webcrm/static/js/streams.js
@@ -2,7 +2,7 @@
 
 const APP_ID = '56f0277797a9410a96f336a003598711';
 const CHANNEL = 'main'
-const AGORA_TOKEN = '007eJxTYAg2/HS4bpPKTZuY5Akymz5LeH9bLGpbJDRRY6V33iOhefMUGEzN0gyMzM3NLc0TLU0MDRItzdKMjc0SDQyMTS0tzA0NLWe/TG0IZGQ4VdfBxMgAgSA+C0NuYmYeAwMA4YYd0Q==';
+const AGORA_TOKEN = '007eJxTYKi9/vpfweWvd8J1TJwTT68sPKb6KvRw0eycq1PzA5cW6rkoMJiapRkYmZubW5onWpoYGiRamqUZG5slGhgYm1pamBsasp19ldoQyMjwVbyBmZEBAkF8FobcxMw8BgYA2zsgaA==';
 let UID;
 console.log("stream.js connected!");
 
@@ -13,16 +13,47 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
-        UID = await client.join(APP_ID, CHANNEL, AGORA_TOKEN, null)
-        console.log("User ID: ", UID);
+    client.on('user-published', handleUserPublished);
+    client.on('user-unpublished', handleUserLeft);
+    UID = await client.join(APP_ID, CHANNEL, AGORA_TOKEN, null)
+    console.warn("User ID: ", UID);
 
-        localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
 
-        let player = `<div class="video-container" id="user-container-${UID}"><div class="username-wrapper"><span class="user-name">Me</span></div><div class="video-player" id="user-${UID}"></div></div>`
-        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+    let player = `<div class="video-container" id="user-container-${UID}"><div class="username-wrapper"><span class="user-name">Me</span></div><div class="video-player" id="user-${UID}"></div></div>`
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
 
-        localTracks[1].play(`user-${UID}`)
-        await client.publish(localTracks[0], localTracks[1])
+    localTracks[1].play(`user-${UID}`)
+    await client.publish(localTracks[0], localTracks[1])
 }
 
-joinAndDisplayLocalStream();
+let handleUserPublished = async (user, mediaType) => {
+
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+    console.warn("User ID: ", user.uid);
+    console.warn("subscribe success");
+    console.warn("'======> User mediaType : ", mediaType);
+    console.warn('======> User published video track:', user.videoTrack);
+    if(mediaType === 'audio') {
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if(player != null) {
+            player.remove()
+        }
+        player = `<div class="video-container" id="user-container-${user.uid}"><div class="username-wrapper"><span class="user-name">User ${user.uid}</span></div><div class="video-player" id="user-${user.uid}"></div></div>`
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+    if (mediaType === 'audio') {
+        user.audioTrack.play()
+    }
+}
+
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    let player = document.getElementById(`user-container-${user.uid}`);
+    if (player) {
+        player.remove();
+    }
+}
+joinAndDisplayLocalStream()
