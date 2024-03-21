db4097e44f11c17ddc3633fae9798dfba7a9f0b1
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-03-21 20:14:48 +0100

    doc: add doc & comments websites backends.py & decorator.py

--- webcrm/website/backends.py
+++ webcrm/website/backends.py
@@ -2,18 +2,47 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 
+# Get the user model
 UserModel = get_user_model()
 
 
 class EmailBackend(ModelBackend):
+    """
+    A custom authentication backend that allows users to log in using their email address or username.
+    Inherits from Django's ModelBackend.
+    """
+
     def authenticate(self, request, username=None, password=None, **kwargs):
+        """
+        Authenticate the user based on email/username and password.
+
+        Parameters:
+        request (HttpRequest): An instance of Django's HttpRequest.
+        username (str): The email or username provided by the user. Default is None.
+        password (str): The password provided by the user. Default is None.
+
+        Returns:
+        user (UserModel instance): The authenticated user. Returns None if authentication fails.
+        """
         try:
-            user = UserModel.objects.get(Q(email__iexact=username) | Q(username__iexact=username))
+            # Try to get the user by email or username (case insensitive)
+            user = UserModel.objects.get(
+                Q(email__iexact=username) | Q(username__iexact=username)
+            )
         except UserModel.DoesNotExist:
+            # If user does not exist, set the password for a new user instance and return None
             UserModel().set_password(password)
             return
         except UserModel.MultipleObjectsReturned:
-            user = UserModel.objects.filter(
-                Q(email__iexact=username) | Q(username__iexact=username)).order_by('id').first()
+            # If multiple users are returned, get the first user based on the 'id' field
+            user = (
+                UserModel.objects.filter(
+                    Q(email__iexact=username) | Q(username__iexact=username)
+                )
+                .order_by("id")
+                .first()
+            )
+        # Check if the password is correct and the user is allowed to authenticate
         if user.check_password(password) and self.user_can_authenticate(user):
+            # Return the authenticated user
             return user
--- webcrm/website/decorators.py
+++ webcrm/website/decorators.py
@@ -2,13 +2,46 @@ from django.shortcuts import redirect
 
 
 def user_not_authenticated(function=None, redirect_url="/"):
+    """
+    Decorator to redirect authenticated users to a specified URL.
+
+    Parameters:
+    function (function): The view function to be decorated. Default is None.
+    redirect_url (str): The URL to redirect to if the user is authenticated. Default is '/'.
+
+    Returns:
+    decorator (function): The decorated view function.
+    """
 
     def decorator(view_func):
+        """
+        Inner decorator function.
+
+        Parameters:
+        view_func (function): The view function to be decorated.
+
+        Returns:
+        _wrapped_view (function): The wrapped view function.
+        """
+
         def _wrapped_view(request, *args, **kwargs):
+            """
+            The actual wrapper function that checks if the user is authenticated.
+
+            Parameters:
+            request (HttpRequest): An instance of Django's HttpRequest.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+            Returns:
+            HttpResponse: The response returned by the view function or a redirect response.
+            """
             if request.user.is_authenticated:
                 return redirect(redirect_url)
             return view_func(request, *args, **kwargs)
+
         return _wrapped_view
+
     if function:
         return decorator(function)
     return decorator
