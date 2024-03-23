64399d1141a5eb57fd17cd40e799bd89797aafa8
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-03-23 08:07:03 +0100

    doc: add doc & comments websites token.py

--- webcrm/website/token.py
+++ webcrm/website/token.py
@@ -2,12 +2,17 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from six import text_type
 
 
+# This class is a custom token generator for account activation.
+# It inherits from Django's PasswordResetTokenGenerator.
 class TokenGenerator(PasswordResetTokenGenerator):
+    # This method is used to create a hash value for the token.
+    # It takes a user object and a timestamp as arguments.
+    # The hash value is created by concatenating the user's primary key,
+    # the timestamp, and the user's active status.
     def _make_hash_value(self, user, timestamp):
-        return (
-                text_type(user.pk) + text_type(timestamp) +
-                text_type(user.is_active)
-        )
+        return text_type(user.pk) + text_type(timestamp) + text_type(user.is_active)
 
 
+# This is an instance of the TokenGenerator class.
+# It is used to generate account activation tokens.
 account_activation_token = TokenGenerator()
