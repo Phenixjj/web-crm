eb8d5fb014726a3588fe935b57c2bf95286f9079
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-03-30 10:45:32 +0100

    doc: add doc & comments webcrm db.py

--- webcrm/webcrm/db.py
+++ webcrm/webcrm/db.py
@@ -2,12 +2,17 @@ import dj_database_url
 
 from .env import config
 
+# Fetch the DATABASE_URL from the environment variables.
+# If it's not set, default to None.
 DATABASE_URL = config("DATABASE_URL", default=None)
+
+# If DATABASE_URL is set, configure the Django database settings.
+# Use dj_database_url to parse the DATABASE_URL and set up the database connection.
+# Set the maximum age of database connections to 600 seconds.
+# Enable connection health checks.
 if DATABASE_URL is not None:
     DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True
+        "default": dj_database_url.config(
+            default=DATABASE_URL, conn_max_age=600, conn_health_checks=True
         )
-}
+    }
